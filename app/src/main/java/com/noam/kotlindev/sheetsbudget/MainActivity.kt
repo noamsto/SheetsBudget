c80c2bfc11a0896e2fed2fe64c9a19ab44bde06f
Noam Stolero <stellern@gmail.com>
2018-12-03 22:17:03 +0200

    initialize commit

---
+++ app/src/main/java/com/noam/kotlindev/sheetsbudget/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.noam.kotlindev.sheetsbudget
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.api.services.sheets.v4.SheetsRequestInitializer
+import com.google.api.services.sheets.v4.model.Spreadsheet
+import kotlin.jvm.internal.SpreadBuilder
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val spreadsheets = Sheets.Spreadsheets
+
+
+    }
+}
